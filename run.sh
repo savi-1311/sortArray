d063b879b74f6f424114147ffac25c8061019ae2
Shambhavi <shambhavishandilya01@gmail.com>
2022-01-14 10:34:04 +0530

    Upload
    Initial files added

---
+++ run.sh
@@ -0,0 +1,26 @@
+# Shell Program to Sort an Array
+clear
+echo "Enter number of elements: "
+read n
+echo "Enter array elements: "
+for ((i=0; i<n; i++))
+do
+	read a[$i]
+done
+for ((i=0; i<n; i++))
+do
+	for ((j=$i; j<n; j++))
+	do
+		if [ ${a[$i]} -gt ${a[$j]} ]
+		then
+			temp=${a[$i]}
+			a[$i]=${a[$j]}
+			a[$i]=$temp
+		fi
+	done
+done
+echo "Sorted Array: "
+for ((i=0; i<n; i++))
+do
+	echo ${a[$i]}
+done
